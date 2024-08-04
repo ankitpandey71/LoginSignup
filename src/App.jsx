2daf36e1b1742bf07cca5a64fd42f90fff233848
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-08-04 17:53:50 +0530

    first cmmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import RefreshHandler from "./utils/RefreshHandler";
+import { useState } from "react";
+import Layout from "./pages/Layout";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            isAuthenticated ? (
+              <Layout>
+                <Home />
+              </Layout>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
